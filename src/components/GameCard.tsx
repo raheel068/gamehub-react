ba5b70238f63f6abc987396f36012988d11d4f2b
Raheel Riaz Malik <raheelmalik068@gmail.com>
2023-07-26 05:36:33 -0400

    Cleanup game card on light mode

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card borderRadius={10} variant="elevated" overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
