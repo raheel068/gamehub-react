30b1eb55e9a5e4a575cbfc491812df41c2f1c4da
Raheel Riaz Malik <raheelmalik068@gmail.com>
2023-07-26 13:59:38 -0400

    Fetch genres and platforms using react query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -19,7 +19,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results?.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <GenreListItem
               genre={genre}
