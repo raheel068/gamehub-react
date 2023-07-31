4db5b82012fae1f34ab0b647a61a6348dd915b38
Raheel Riaz Malik <raheelmalik068@gmail.com>
2023-07-31 18:21:29 -0400

    Handle errors

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,12 +1,29 @@
-import { Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
+
   return (
     <>
       <NavBar />
-      <Heading>Opps sorry</Heading>
-      <Text>Invalid page</Text>
+      <Flex
+        justifyContent="center"
+        flexDirection="column"
+        alignItems="center"
+        height="100vh"
+      >
+        <Heading as="h1" marginBottom={5}>
+          Error
+        </Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? "This page doesn't not exist."
+            : "An unexpected error occurred."}
+        </Text>
+      </Flex>
     </>
   );
 };
