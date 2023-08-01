87ceb77e9c07d3929f3581c0c8d22d8bd82fa7f0
Raheel Riaz Malik <raheelmalik068@gmail.com>
2023-08-01 18:40:23 -0400

    Refactor - apply game detail link to game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,20 +11,20 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} variant="elevated" overflow="hidden">
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          <Link to={"/games/" + game.slug}>{game.name}</Link>
-        </Heading>
-      </CardBody>
-    </Card>
+    <Link to={"/games/" + game.slug}>
+      <Card borderRadius={10} variant="elevated" overflow="hidden">
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
